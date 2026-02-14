f0e4dd7acf079e5024605730ddc0ada340651086
lleonards <lleonards.777@gmail.com>
2026-02-14 14:56:51 -0300

    Update projects.js

--- LenaVS_Backend/src/routes/projects.js
+++ LenaVS_Backend/src/routes/projects.js
@@ -6,23 +6,56 @@ import {
   updateProject,
   deleteProject
 } from '../controllers/projectController.js';
+
 import { authenticateToken } from '../middleware/auth.js';
+import { requireActiveAccess } from '../middleware/requireActiveAccess.js';
 
 const router = express.Router();
 
+/*
+  🔐 TODAS as rotas exigem:
+  1. Usuário autenticado
+  2. Trial ativo OU assinatura ativa
+*/
+
 // Criar novo projeto
-router.post('/', authenticateToken, createProject);
+router.post(
+  '/',
+  authenticateToken,
+  requireActiveAccess,
+  createProject
+);
 
 // Listar projetos do usuário
-router.get('/', authenticateToken, listProjects);
+router.get(
+  '/',
+  authenticateToken,
+  requireActiveAccess,
+  listProjects
+);
 
 // Obter projeto específico
-router.get('/:id', authenticateToken, getProject);
+router.get(
+  '/:id',
+  authenticateToken,
+  requireActiveAccess,
+  getProject
+);
 
 // Atualizar projeto
-router.put('/:id', authenticateToken, updateProject);
+router.put(
+  '/:id',
+  authenticateToken,
+  requireActiveAccess,
+  updateProject
+);
 
 // Deletar projeto
-router.delete('/:id', authenticateToken, deleteProject);
+router.delete(
+  '/:id',
+  authenticateToken,
+  requireActiveAccess,
+  deleteProject
+);
 
 export default router;
