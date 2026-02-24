9cef86c477af9b4a4a5b33f6210763d13cc47ed0
lleonards <lleonards.777@gmail.com>
2026-02-23 23:39:37 -0300

    Update stripe.js

--- LenaVS_Backend/src/config/stripe.js
+++ LenaVS_Backend/src/config/stripe.js
@@ -1,4 +1,11 @@
 import Stripe from "stripe";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+if (!process.env.STRIPE_SECRET_KEY) {
+  console.error("❌ STRIPE_SECRET_KEY não encontrada nas variáveis de ambiente.");
+}
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
   apiVersion: "2023-10-16",
